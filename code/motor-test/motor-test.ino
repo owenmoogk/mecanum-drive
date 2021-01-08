90fbac8c98fb63103e3d0ca883c8714c974afa96
owenmoogk <owenmoogk@gmail.com>
2021-01-08 00:22:38 -0500

    changed file structure, added motor stuff

--- code/motor-test/motor-test.ino
+++ code/motor-test/motor-test.ino
@@ -4,7 +4,4 @@ void setup() {
 }
 void loop() {
   digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
 }
