34a1315fa817e1a30a2d12513fa485c34fee15b6
owenmoogk <owenmoogk@gmail.com>
2021-01-07 22:58:16 -0500

    added motor test code

---
+++ code/motor-test/motor-test.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+}
+void loop() {
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
